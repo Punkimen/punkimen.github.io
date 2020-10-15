614ad906d54adf117a5cb57f5e070e6a70b72501
max <punkimen666@gmail.com>
2020-10-15 15:19:34 +0300

    d

---
+++ EasyCash/scripts/resize.js
@@ -0,0 +1,8 @@
+if ($(window).width() <= '1120') {
+    $('.tabs__name-case').addClass('laptop')
+}
+function windowSize() {
+    if ($(window).width() <= '1120') {
+        $('.tabs__name-case').addClass('laptop')
+    }
+}
