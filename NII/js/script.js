ba9e6778e88081077f93cdfe422ba751e36ae982
max <punkimen666@gmail.com>
2021-03-13 15:12:35 +0300

    adaptive media

--- NII/js/script.js
+++ NII/js/script.js
@@ -1,11 +1,63 @@
 $(document).ready(function () {
+    //   общие функции
+    const equalHeight = (elem) => {
+        let max_col_height = 0;
+        elem.each(function () {
+            if ($(this).height() > max_col_height) {
+                max_col_height = $(this).height();
+            }
+        });
+        elem.height(max_col_height);
+    }
+    //   общие функции end
+
+    $('.burger__icon').on('click', function () {
+        $(this).toggleClass('active');
+        $('.header-mobile').toggleClass('show');
+    });
+    $('.header-mobile__link--more').on('click', function () {
+        $(this).toggleClass('open')
+        $(this).next().slideToggle();
+        return false
+    })
+    $('.sublist-elem--more').on('click', function () {
+        $(this).toggleClass('open')
+        $(this).next().slideToggle();
+        return false
+    })
+
+
+
+
     $('.reviews-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         prevArrow: '<div class="slider-arrow slider-arrow--prev"></div>',
         nextArrow: '<div class="slider-arrow slider-arrow--next"></div>',
+        responsive: [
+            {
+                breakpoint: 568,
+                settings: {
+                    arrows: false,
+                }
+            },
+        ]
     })
     $('.team__all-scroll').mCustomScrollbar({
         axis: "y",
     });
+    $(window).resize(function () {
+        let windowWidth = $(window).width();
+        if (windowWidth > 767) {
+            equalHeight($('.footer-title'))
+        }
+        equalHeight($('.footer-title'))
+    })
+    // вызов общих функций
+    if ($(window).width() > 767) {
+        equalHeight($('.footer-title'))
+    }
+    equalHeight($('.footer-title'))
+
+    // вызов общих функций end
 });
