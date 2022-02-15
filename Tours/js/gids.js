1651ebd9eddbcf5f56bf5bdc18e7548b45b38b76
Punkimen <66203124+Punkimen@users.noreply.github.com>
2022-02-15 18:37:27 +0300

    adaptive fixed

--- Tours/js/gids.js
+++ Tours/js/gids.js
@@ -1,67 +1,54 @@
 (function () {
-  let swiperGids;
-  const sliderInit = () => {
-    const windowWidth = $(window).width();
-    if (windowWidth <= 992) {
-      $('.gids-item').addClass('swiper-slide')
-      swiperGids = new Swiper('.gids-slide', {
-        loop: false,
-        spaceBetween: 15,
-        slidesPerView: 1,
-        autoHeigh: true,
-        breakpoints: {
 
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 30
-          }
-        }
-      });
+  const swiperGids = new Swiper('.gids-slide', {
+    loop: false,
+    spaceBetween: 15,
+    slidesPerView: 1,
+    autoHeigh: true,
+    breakpoints: {
 
-      let count = swiperGids.activeIndex + 1;
-      let countMax = $('.gids-item').length;
-      function sliderCountSet(count) {
-        $('#start-slide-gids').text(count)
-        $('#end-slide-gids').text(countMax)
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30
       }
-      sliderCountSet()
-      function sliderCountPlus() {
-        if (count < countMax) {
-          count++
-          sliderCountSet(count)
-        }
-      }
-      function sliderCountMinus() {
-        if (count > 1) {
-          count--
-          sliderCountSet(count)
-        }
-      }
-
-      $('#gids-slider__prev').on('click', function () {
-        swiperGids.slidePrev()
-        sliderCountMinus()
-      })
-      $('#gids-slider__next').on('click', function () {
-        swiperGids.slideNext()
-        sliderCountPlus()
-      })
-
-      swiperGids.on('slideChange', function () {
-        sliderCountSet(swiperGids.activeIndex + 1)
-      });
-    } else {
-      $('.gids-item').removeClass('swiper-slide')
-      swiperGids = null
     }
+  });
 
-
-
+  let count = swiperGids.activeIndex + 1;
+  let countMax = $('.gids-item').length;
+  function sliderCountSet(count) {
+    $('#start-slide-gids').text(count)
+    $('#end-slide-gids').text(countMax)
   }
-  $(window).on('resize', function () {
-    sliderInit()
-  })
-  sliderInit()
+  sliderCountSet()
+  function sliderCountPlus() {
+    if (count < countMax) {
+      count++
+      sliderCountSet(count)
+    }
+  }
+  function sliderCountMinus() {
+    if (count > 1) {
+      count--
+      sliderCountSet(count)
+    }
+  }
 
+  $('#gids-slider__prev').on('click', function () {
+    swiperGids.slidePrev()
+    sliderCountMinus()
+  })
+  $('#gids-slider__next').on('click', function () {
+    swiperGids.slideNext()
+    sliderCountPlus()
+  })
+
+  swiperGids.on('slideChange', function () {
+    sliderCountSet(swiperGids.activeIndex + 1)
+  });
 })();
 
