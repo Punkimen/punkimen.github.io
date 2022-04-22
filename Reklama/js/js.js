a7553b728da6665152c9b990c8726e2143da4f8a
Punkimen <66203124+Punkimen@users.noreply.github.com>
2022-04-22 22:51:59 +0300

    s

--- Reklama/js/js.js
+++ Reklama/js/js.js
@@ -1,40 +1,48 @@
-const menuBtn = document.querySelector('.menu-button')
-const menuContainer = document.querySelector('.menu-container')
-const leftMenu = document.querySelector('.menu-contacts')
-const rightMenu = document.querySelector('.menu')
 
-console.log(leftMenu.style.height);
-console.log(rightMenu.offsetHeight);
 
-function setEqualHeight(columns) {
-  const windowWidth = $(window).width()
-  var tallestcolumn = 0;
-
-  columns.each(function () {
-    currentHeight = $(this).height();
-    if (currentHeight > tallestcolumn) {
-      tallestcolumn = currentHeight;
-    }
+$(document).ready(function () {
+  AOS.init({
+    easing: 'ease-out-back',
   });
+  const menuBtn = document.querySelector('.menu-button')
+  const menuContainer = document.querySelector('.menu-container')
+  const leftMenu = document.querySelector('.menu-contacts')
+  const rightMenu = document.querySelector('.menu')
 
-  if (windowWidth >= 1020) {
-    columns.height(tallestcolumn);
-  } else {
-    columns.css({ 'height': 'auto' })
-    return false;
+  console.log(leftMenu.style.height);
+  console.log(rightMenu.offsetHeight);
+
+  function setEqualHeight(columns) {
+    const windowWidth = $(window).width()
+    var tallestcolumn = 0;
+
+    columns.each(function () {
+      currentHeight = $(this).height();
+      if (currentHeight > tallestcolumn) {
+        tallestcolumn = currentHeight;
+      }
+    });
+
+    if (windowWidth >= 1020) {
+      columns.height(tallestcolumn);
+    } else {
+      columns.css({ 'height': 'auto' })
+      return false;
+    }
   }
-}
 
 
 
-const toggleMenu = () => {
-  document.querySelector('body').classList.toggle('overlay')
-  menuContainer.classList.toggle('active')
-  leftMenu.classList.toggle('active')
-  rightMenu.classList.toggle('active')
-  setEqualHeight($(".column"));
-}
 
-menuBtn.addEventListener('click', e => {
-  toggleMenu()
+  const toggleMenu = () => {
+    document.querySelector('body').classList.toggle('overlay')
+    menuContainer.classList.toggle('active')
+    leftMenu.classList.toggle('active')
+    rightMenu.classList.toggle('active')
+    setEqualHeight($(".column"));
+  }
+
+  menuBtn.addEventListener('click', e => {
+    toggleMenu()
+  })
 })
