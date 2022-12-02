454912aafa8257e6aa6211eaccfb2b3d8a578f85
Punkimen <66203124+Punkimen@users.noreply.github.com>
2022-12-02 18:08:49 +0300

    add anchor and animation and gallery

--- Atom/js/script.js
+++ Atom/js/script.js
@@ -1,4 +1,5 @@
 "use strict";
+// Подключения слайдера
 const swiper = new Swiper(".swiper", {
 	slidesPerView: 1,
 	loop: true,
@@ -51,3 +52,52 @@ const swiper = new Swiper(".swiper", {
 		},
 	},
 });
+
+// Бургер меню
+
+const headerMobile = document.querySelector(".header-mobile");
+const headerNav = document.querySelector(".header__nav");
+const headerLogo = document.querySelector(".header__logo");
+const burgerBtn = document.querySelector(".burger__icon");
+
+const toogleClass = (el, className) => {
+	console.log(el.classList.toggle(className));
+};
+const removeClass = (el, className) => {
+	console.log(el.classList.remove(className));
+};
+const adaptiveMenu = () => {
+	const windowWidth = window.innerWidth;
+	if (windowWidth <= 1200) {
+		headerMobile.querySelector(".container").prepend(headerNav);
+	} else {
+		headerLogo.after(headerNav);
+	}
+};
+adaptiveMenu();
+window.addEventListener("resize", adaptiveMenu);
+
+burgerBtn.addEventListener("click", (e) => {
+	toogleClass(burgerBtn, "active");
+	toogleClass(headerMobile, "show");
+});
+
+// Якорные ссылки
+const smoothLinks = document.querySelectorAll("[scroll-href]");
+console.log(smoothLinks);
+for (let smoothLink of smoothLinks) {
+	smoothLink.addEventListener("click", function (e) {
+		e.preventDefault();
+		const id = smoothLink.getAttribute("scroll-href");
+		removeClass(burgerBtn, "active");
+		removeClass(headerMobile, "show");
+		document.querySelector(id).scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+		});
+	});
+}
+
+// Галерея сертификатов
+
+lightGallery(document.getElementById("sertivicates-gallery"));
