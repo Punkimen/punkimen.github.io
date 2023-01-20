18c57988a525c6c97ba40e273a30039ce6e11253
punkimen <punkimen666@gmail.com>
2023-01-20 13:26:02 +0300

    deleteSites

--- TestCamera/scripts/resize.js
+++
@@ -1,8 +0,0 @@
-if ($(window).width() <= '1120') {
-    $('.tabs__name-case').addClass('laptop')
-}
-function windowSize() {
-    if ($(window).width() <= '1120') {
-        $('.tabs__name-case').addClass('laptop')
-    }
-}
