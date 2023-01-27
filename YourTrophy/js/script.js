ed0386acdd631b48113a17e6975969eefd455074
punkimen <punkimen666@gmail.com>
2023-01-27 20:06:07 +0300

    Fishing

--- YourTrophy/js/script.js
+++ YourTrophy/js/script.js
@@ -1,5 +1,19 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function () {
+window.onload = function () {
+	document.body.classList.add("loaded_hiding");
+	window.setTimeout(function () {
+		document.body.classList.add("loaded");
+		document.body.classList.remove("loaded_hiding");
+	}, 500);
+	const lazyLoadInstance = new LazyLoad({
+		use_native: true,
+	});
+	AOS.init({
+		offset: 20,
+	});
+	lazyLoadInstance.update();
+};
+window.document.addEventListener("DOMContentLoaded", function () {
 	const addClass = (el, className) => {
 		el.classList.add(className);
 	};
@@ -40,5 +54,69 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	window.addEventListener("resize", (e) => {
 		headerMobileBuild();
+	});
+
+	// Якорные ссылки
+
+	const smoothLinks = document.querySelectorAll("[scroll-href]");
+
+	for (let smoothLink of smoothLinks) {
+		smoothLink.addEventListener("click", function (e) {
+			e.preventDefault();
+			const id = smoothLink.getAttribute("scroll-href");
+			removeClass(burger, "active");
+			removeClass(mobileMenu, "open");
+			removeClass(document.body, "overlay");
+			const el = document.querySelector(id);
+			if (el) {
+				el.scrollIntoView({
+					behavior: "smooth",
+					block: "start",
+				});
+			} else {
+				window.history.back();
+			}
+		});
+	}
+
+	// Select
+	customSelect("select");
+
+	// popups
+	const popups = document.querySelector(".popups");
+	const popupsElems = document.querySelectorAll(".popup");
+	const closeBtn = document.querySelectorAll(".js-close-popup");
+	const popupsOpenBtns = document.querySelectorAll("[data-popup]");
+
+	const showPopup = (selector) => {
+		addClass(popups, "show");
+		const el = document.querySelector(selector);
+		addClass(el, "show");
+	};
+	const closePopup = () => {
+		removeClass(popups, "show");
+		popupsElems.forEach((el) => {
+			removeClass(el, "show");
+		});
+	};
+
+	popupsOpenBtns.forEach((el) => {
+		el.addEventListener("click", (e) => {
+			const popupSelector = el.getAttribute("data-popup");
+			showPopup(popupSelector);
+		});
+	});
+
+	if (closeBtn.length > 0) {
+		closeBtn.forEach((el) => {
+			el.addEventListener("click", () => {
+				closePopup();
+			});
+		});
+	}
+	document.addEventListener("click", (e) => {
+		if (e.target.className === "popups__inner") {
+			closePopup();
+		}
 	});
 });
