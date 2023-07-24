26d2ad0b6e5c18e64afcee913dede3a5734c612d
Punkimen <66203124+Punkimen@users.noreply.github.com>
2023-07-24 10:25:16 +0300

    123

--- ONMI/js/module/adaptiveResize.js
+++ ONMI/js/module/adaptiveResize.js
@@ -52,43 +52,45 @@ export const cardsAdaptive = () => {
         cardsWrapper.append(card3)
         cardsWrapper.append(card4)
         cardsWrapper.append(card2)
-    } else {
-        removeClass(cardsWrapper, 'wrapper-row')
-        cardsColumns.forEach((el, index) => {
-            if (index === 0) {
-                cardsColumns[index].append(card1, card2)
-            }
-            if (index === 1) {
-                cardsColumns[index].append(card3, card4)
-            }
-        })
+    } else if (windowWidth > 768 && cardsWrapper.classList.contains('wrapper-row')) {
+        {
+            removeClass(cardsWrapper, 'wrapper-row')
+            cardsColumns.forEach((el, index) => {
+                if (index === 0) {
+                    cardsColumns[index].append(card1, card2)
+                }
+                if (index === 1) {
+                    cardsColumns[index].append(card3, card4)
+                }
+            })
+        }
     }
-}
 
-export const journeyCardsAdaptive = () => {
+    export const journeyCardsAdaptive = () => {
 
-    const windowWidth = window.innerWidth
-    const cardsWrapper = document.querySelector('.journey-cards .journey-cards__wrapper')
-    const card1 = cardsWrapper?.querySelector('.journey-card_1')
-    const card2 = cardsWrapper?.querySelector('.journey-card_2')
-    const card3 = cardsWrapper?.querySelector('.journey-card_3')
-    const card4 = cardsWrapper?.querySelector('.journey-card_4')
-    const cardsColumns = cardsWrapper?.querySelectorAll('.cards__col')
-    if (windowWidth <= 768 && !cardsWrapper.classList.contains('wrapper-row')) {
-        addClass(cardsWrapper, 'wrapper-row')
-        cardsWrapper.append(card1)
-        cardsWrapper.append(card2)
-        cardsWrapper.append(card3)
-        cardsWrapper.append(card4)
-    } else {
-        removeClass(cardsWrapper, 'wrapper-row')
-        cardsColumns.forEach((el, index) => {
-            if (index === 0) {
-                cardsColumns[index].append(card1, card3)
-            }
-            if (index === 1) {
-                cardsColumns[index].append(card2, card4)
-            }
-        })
+        const windowWidth = window.innerWidth
+        const cardsWrapper = document.querySelector('.journey-cards .journey-cards__wrapper')
+        const card1 = cardsWrapper?.querySelector('.journey-card_1')
+        const card2 = cardsWrapper?.querySelector('.journey-card_2')
+        const card3 = cardsWrapper?.querySelector('.journey-card_3')
+        const card4 = cardsWrapper?.querySelector('.journey-card_4')
+        const cardsColumns = cardsWrapper?.querySelectorAll('.cards__col')
+        if (windowWidth <= 768 && !cardsWrapper.classList.contains('wrapper-row')) {
+            addClass(cardsWrapper, 'wrapper-row')
+            cardsWrapper.append(card1)
+            cardsWrapper.append(card2)
+            cardsWrapper.append(card3)
+            cardsWrapper.append(card4)
+        } else if
+        (windowWidth > 768 && cardsWrapper.classList.contains('wrapper-row')) {
+            removeClass(cardsWrapper, 'wrapper-row')
+            cardsColumns.forEach((el, index) => {
+                if (index === 0) {
+                    cardsColumns[index].append(card1, card3)
+                }
+                if (index === 1) {
+                    cardsColumns[index].append(card2, card4)
+                }
+            })
+        }
     }
-}
